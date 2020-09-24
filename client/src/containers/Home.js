2a384fe97a26de8b7419b9b8f500fdfbac1af940
MrPoketes <az778@york.ac.uk>
2020-09-24 16:34:08 +0100

    initial setup

---
+++ client/src/containers/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "../css/styles.css";
+
+class Home extends Component{
+  render(){
+    return(
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
+
+export default Home;
